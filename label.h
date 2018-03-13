e7f791889241495d9f390d1680451e086e1f6587
Quan Wang <BetterWang@gmail.com>
2018-03-13 15:21:41 +0100

    [+] label

--- label.h
+++ label.h
@@ -1,12 +1,20 @@
 
 char const * fname[] = {
 	"test/",		// 0 test
-	"../PbPb2015_cumu/HIMinimumBias2/crab_HIMB2_V1Cumu_pixel_eff_v1/180310_171720/0000/",		// 1 PbPb15 Pixel
+	"../PbPb2015_cumu/HIMinimumBias2/crab_HIMB2_V1Cumu_pixel_eff_v3/180313_053437/0000/",			// 1 PbPb15 Pixel
+	"../PbPb2015_cumu/HIMinimumBias2/crab_HIMB2_V1Cumu_pixel_eta08_eff_v3/180313_053552/0000/",		// 2 PbPb15 Pixel eta08
+	"",		// 3 PbPb15 MB5 ppReco
+	"../PbPb2015_cumu/HIMinimumBias6/crab_HIMB6_V1Cumu_ppReco_eff_v3/180313_054131/0000/",			// 4 PbPb15 MB6 ppReco
+	"../PbPb2015_cumu/HIMinimumBias7/crab_HIMB7_V1Cumu_ppReco_eff_v3/180313_054158/0000/",			// 5 PbPb15 MB7 ppReco
 };
 
 char const * ftxt[] = {
 	"txt/test/",			// 0
 	"txt/PbPb15_HIMB2/",		// 1
+	"txt/PbPb15_HIMB2_eta08/",		// 2
+	"txt/PbPb15_ppReco5/",			// 3
+	"txt/PbPb15_ppReco6/",			// 4
+	"txt/PbPb15_ppReco7/",			// 5
 };
 
 #include <TChain.h>
