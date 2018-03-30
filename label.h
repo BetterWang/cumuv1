8fc604205f9198887c3dfdd4a4d5877c185619a0
Quan Wang <BetterWang@gmail.com>
2018-03-30 23:17:03 +0200

    [+] label

--- label.h
+++ label.h
@@ -28,7 +28,8 @@ char const * fname[] = {
 	"../PbPb2015_cumu/HIMinimumBias7/crab_HIMB7_V1Cumu_ppReco_poseta_effacc_b12_v5/180318_204018/0000/",	// 25 PbPb15 MB7 ppReco acc poseta b12
 	"NA",		// 26 PbPb15 MB567 ppReco acc poseta b12
 	"../PbPb2015_cumu/AMPT_PbPb5TeV_Gen/crab_AMPT1_V1_v1/180326_211451/0000/",				// 27 AMPT b12
-	"../PbPb2015_cumu/MC/PionExpoPtV2/",									// 28 ToyMC b12
+	"../PbPb2015_cumu/MC/PionExpoPtV2/",									// 28 ToyMC V2 b12
+	"../PbPb2015_cumu/MC/PionExpoPtV1V2/",									// 29 ToyMC V1 V2 b12
 };
 
 char const * ftxt[] = {
@@ -61,6 +62,7 @@ char const * ftxt[] = {
 	"txt/PbPb15_ppReco567_poseta_b12_acc/",	// 26
 	"txt/AMPT_b12/",			// 27
 	"txt/ToyMC_b12/",			// 28
+	"txt/ToyMC_V1V2_b12/",			// 29
 };
 
 #include <TChain.h>
